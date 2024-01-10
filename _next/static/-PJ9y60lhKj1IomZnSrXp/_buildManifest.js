335347fa25ce174897dc97aff9c08772ae5e4ccf
s1f102103707 <s1f102103707@users.noreply.github.com>
2024-01-10 08:35:21 +0000

    deploy: 3cd1bb0442662447a160aa496c7219cc15b8c770

---
+++ _next/static/-PJ9y60lhKj1IomZnSrXp/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/30d1ecabd9ab3dad.css","static/chunks/pages/index-df4ad13f91db8bf7.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
